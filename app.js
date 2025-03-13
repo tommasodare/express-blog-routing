6336185bba98dc08644aef962a5af2a49f39cdb1
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-13 14:44:49 +0100

    Import Router in app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const postsRouter = require('./routers/posts')
+
+app.use('/api/v1/posts', postsRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`);
